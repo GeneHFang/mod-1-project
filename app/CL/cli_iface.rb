0eccc3583b8cbe5e817136db8df50c5d0fe191df
GeneHFang <ghl234@nyu.edu>
2019-10-21 16:55:51 -0400

    Functioning Session creation implemented

--- app/CL/cli_iface.rb
+++ app/CL/cli_iface.rb
@@ -6,3 +6,30 @@ main_prompt = TTY::Prompt.new
 selection = main_prompt.select("What do you want to do?", %w(Play Exit))
 end
 
+#returns results in form of Hash { score : value, times_played: value }
+def cli_runner
+    
+    choice = main_menu
+    abort("Ending now") if choice == "Exit"
+    returnHash = {}
+    returnHash[:score] = 0
+    returnHash[:times_played] = 0
+    start_time = Time.now
+    end_time = start_time + 10
+    while true do
+        current_time = Time.now
+        #end immediately if time is up
+        break if current_time > end_time
+    
+        returnHash[:times_played] += 1
+    
+        returnHash[:score]+=1 if start_rps 
+        
+        puts "="*70
+        
+        
+    end
+    
+    returnHash
+
+end
--- bin/run.rb
+++ bin/run.rb
@@ -1,30 +1,16 @@
 require_relative '../config/environment'
 require_all 'app'
 
-x = 0
-choice = main_menu
-abort("Ending now") if choice == "Exit"
-start_time = Time.now
-end_time = start_time + 10
-while true do
-    current_time = Time.now
-    #end immediately if time is up
-    break if current_time > end_time
 
-    x+=1 if start_rps 
-    
-    puts "="*70
-    
-    
-end
+sessionResult = cli_runner
+
+stat1 = Gamestat.create(rps_score: sessionResult[:score], rps_times: sessionResult[:times_played])
+
+user1 = User.find_by(name: "Gene")
 
 
+Session.create(user: user1, gamestat: stat1)
 
-# p = TTY::Prompt.new
-# cpu_move = pick_rps
-# s = p.select(cpu_move, %w(✊ ✌️ 👋))
-
-# result = winner?(cpu_move,s)
 
 binding.pry
 
